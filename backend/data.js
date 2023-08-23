a7006aec423e261db0477059b67d17789b48528d
Jamajal <leandroliberiomachado@hotmail.com>
2023-08-23 18:21:45 -0300

    Criando servidor com express

---
+++ backend/data.js
@@ -0,0 +1,22 @@
+const posts = [
+  {
+    title: 'League of Legends',
+    type: 'Moba',
+    description: 'adsçlkjadfsadfjlasdfçasdkflçasdfkasfdjadsçlkfsa',
+    comments: ['Legal', 'Uma merda', 'Não quero jogar', 'Main ez é top'],
+  },
+  {
+    title: 'Dota',
+    type: 'Moba',
+    description: 'adsçlkjadfsadfjlasdfçasdkflçasdfkasfdjadsçlkfsa',
+    comments: ['Legal', 'Uma merda', 'Não quero jogar', 'Main ez é top'],
+  },
+  {
+    title: 'Starcraft',
+    type: 'Moba',
+    description: 'adsçlkjadfsadfjlasdfçasdkflçasdfkasfdjadsçlkfsa',
+    comments: ['Legal', 'Uma merda', 'Não quero jogar', 'Main ez é top'],
+  },
+];
+
+export default posts;
