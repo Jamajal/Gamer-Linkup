91a958fb6cb3711cd4b5df0b8649191d6a5fad09
Jamajal <leandroliberiomachado@hotmail.com>
2023-09-04 19:24:54 -0300

    Criando branch para login

--- frontend/src/components/Post/Post.tsx
+++ frontend/src/components/Post/Post.tsx
@@ -1,35 +1,39 @@
-import './Post.scss'
-import gameImage from '../../assets/game-img.png'
+import './Post.scss';
+import gameImage from '../../assets/game-img.png';
 
 const Post = () => {
   return (
-    <div className='app__post'>
+    <div className="app__post">
       <figure>
-        <img src={gameImage}/>
+        <img src={gameImage} />
         <figcaption>LEAGUE OF LEGENDS</figcaption>
-        <div className='app__post-imgshadow'></div>
+        <div className="app__post-imgshadow"></div>
       </figure>
-      <div className='app__post-content'>
+      <div className="app__post-content">
         <p>
-          Lore IPSUM LORE IPSUM IPSUM LORE Lore IPSUM LORE IPSUM IPSUM LORE
-          Lore IPSUM LORE IPSUM IPSUM LORE Lore IPSUM LORE IPSUM IPSUM LORE 
-          Lore IPSUM LORE IPSUM IPSUM LORE Lore IPSUM LORE IPSUM IPSUM LORE 
-          Lore IPSUM LORE IPSUM IPSUM LORE Lore IPSUM LORE IPSUM IPSUM LORE 
-          Lore IPSUM LORE IPSUM 
+          Lore IPSUM LORE IPSUM IPSUM LORE Lore IPSUM LORE IPSUM IPSUM LORE Lore
+          IPSUM LORE IPSUM IPSUM LORE Lore IPSUM LORE IPSUM IPSUM LORE Lore
+          IPSUM LORE IPSUM IPSUM LORE Lore IPSUM LORE IPSUM IPSUM LORE Lore
+          IPSUM LORE IPSUM IPSUM LORE Lore IPSUM LORE IPSUM IPSUM LORE Lore
+          IPSUM LORE IPSUM
         </p>
 
         <div>
-          <div className='app__post-footer'>
-            <input type='button'/><span>145</span>
-            <input type='button'/><span>30</span>
-            <input type='button'/><span>10</span>
-            <input type='button'/><span>4 PLAYERS</span>
+          <div className="app__post-footer">
+            <input type="button" />
+            <span>145</span>
+            <input type="button" />
+            <span>30</span>
+            <input type="button" />
+            <span>10</span>
+            <input type="button" />
+            <span>4 PLAYERS</span>
           </div>
           <span>nome do autor</span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
